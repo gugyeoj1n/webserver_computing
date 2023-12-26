62ba7d3883b392bc434d75d6342c4d450d435083
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-12-26 11:31:20 +0900

    2일차 | Question, Answer 마이그레이션

--- projects/mysite/pybo/models.py
+++ projects/mysite/pybo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Question(models.Model) :
     subject     = models.CharField(max_length = 200)
     content     = models.TextField()
